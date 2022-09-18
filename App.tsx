d85f8f9e8b8f165b33acc2cb8789da9218662f38
Pedro Paulo <pedropaulobrasca@gmail.com>
2022-09-18 18:14:48 -0300

    Notification

--- App.tsx
+++ App.tsx
@@ -6,10 +6,16 @@ import {
   Inter_700Bold,
   Inter_900Black,
 } from "@expo-google-fonts/inter";
+import { useRef, useEffect } from "react";
+import * as Notifications from "expo-notifications";
 
 import { Background } from "./src/components/Background";
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+
+import "./src/services/notificationConfigs";
+import { Subscription } from "expo-modules-core";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,6 +24,30 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const resNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log("Notification received in foreground", notification);
+      });
+
+    resNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log("Notification response received in foreground", response);
+      });
+
+    return () => {
+      getNotificationListener.current?.remove();
+      resNotificationListener.current?.remove();
+    };
+  }, []);
 
   return (
     <Background>
---
+++ src/services/getPushNotificationToken.ts
@@ -0,0 +1,17 @@
+import * as Notifications from "expo-notifications";
+
+export async function getPushNotificationToken() {
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== "granted") {
+    alert("Failed to get push token for push notification!");
+    return;
+  }
+  const token = (await Notifications.getExpoPushTokenAsync()).data;
+  console.log("NOTIFICATION TOKEN: ", token);
+  return token;
+}
